a1d3722a152d7d4e761b05aa48336af9c4baf89b
Jesper Niemann <jesp23c6@tietgen.dk>
2020-04-14 11:45:06 +0200

    Added construct and constants

--- classes/SysDB.php
+++ classes/SysDB.php
@@ -2,7 +2,26 @@
 
 class SysTem{
 
-    
+    public $conn;
+
+    const $DB_NAME;
+
+    const $DB_USER;
+
+    const $DB_PASSWORD;
+
+    const $DB_HOST;
+
+    public function __construct(){
+
+        $this->conn = new mysqli ("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME");
+
+        if ($this->conn->connect_error){
+            die('Error connecting to the database: ' . $this->conn->connect_error);
+        }
+        $this->conn->set_charset("utf-8");
+
+    }
 
 }
 
