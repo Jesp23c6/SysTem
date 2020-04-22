266cbeb3d7b7476b554212696b984bf13b8589a9
Jesper Niemann <jesp23c6@tietgen.dk>
2020-04-22 12:29:21 +0200

    [HOTFIX] removed unnecessary code.

--- classes/SysDB.php
+++ classes/SysDB.php
@@ -169,56 +169,6 @@ class SysDB{
      * 
      * @param   array $data
      * 
-     * @return  array $result
-     * 
-     * $data is expected to be a numbered array with associative arrays within.
-     * 
-     * $a_key_array and $a_value_array contain all the keys and values in the associative arrays respectively as they get array_pushed in the foreach loops.
-     */
-    function insert_first($table_name, $data){
-
-        foreach($data as $key => $value){
-
-            $a_key_array = array();
-
-            $a_value_array = array();
-
-            foreach($value as $a_key => $a_value){
-
-                array_push($a_key_array, $a_key); 
-
-                array_push($a_value_array, $a_value);
-
-            }
-
-            $sql = "INSERT INTO $table_name (" . implode(', ', $a_key_array) . ") VALUES ('" . implode("', '", $a_value_array) . "')";
-
-            $query = $this->conn->query($sql);
-
-            if($query){
-
-                $result = true;
-    
-            }
-            else{
-    
-                $result = false;
-    
-            }
-        
-        }
-
-        return $result;
-
-    }
-
-    /**
-     * A method for inserting data into a table.
-     * 
-     * @param   string $table_name
-     * 
-     * @param   array $data
-     * 
      * $data is expected to be a numbered array with associative arrays within.
      * 
      * $a_key_array and $a_value_array contain all the keys and values in the associative arrays respectively as they get array_pushed in the foreach loops.
@@ -374,67 +324,7 @@ class SysDB{
         
     }
 
-
-    /**
-     * A method for updating based on input.
-     * 
-     * @param   string $table_name
-     * @param   array $data
-     * @param   array $where
-     * 
-     */
-    function get_update($table_name, $data, $where){
-
-        $columns = array();
-
-        foreach($data as $col => $val){
-
-            $values = $col . " = " . "'" . $val . "'";
-
-            array_push($columns, $values);
-
-        }
-
-        $updates = implode(", ", $columns);
-
-        $sql = "UPDATE $table_name SET $updates WHERE ";
-
-        $and_count = count($where);
-
-        //At this point I do a count to check how many ANDs should be implemented.
-        if($and_count - 1 / 1 > 0){
-
-            $and_counter = $and_count -1 / 1;
-
-            foreach($where as $and_array_key => $and_array_val){
-
-                $sql = $sql . $and_array_key . " = " . "'" . $and_array_val . "'";
-
-                //Here's where the ANDs get implemented if the counter is higher than 0.
-                if($and_counter > 0){
-
-                    $sql = $sql . " AND ";
-
-                    $and_counter = $and_counter -1;
-
-                }
-
-            }
-
-        }
-        else{
-            
-            foreach($where as $and_array_key => $and_array_val){
-
-                $sql = $sql . $and_array_key . " = " . "'" . $and_array_val . "'";
-
-            }
-            
-        }
-        
-        $query = $this->conn->query($sql);
-
-    }
+    
 
 }
 
